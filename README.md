ee3ef78b760f2f3ea7183ed0df8838ced48c6ace
jjl <james@behemoth>
2017-09-05 08:58:07 +0200

    last error?

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ The irresponsible software guild presents...
 
 # argon2_erl
 
-The argon2 secure password hashing function, now usable from your 
+The argon2 secure password hashing function, now usable from your rebar3 project
 
 ## Usage:
 
@@ -17,9 +17,11 @@ And this to your `.app.src`'s `applications`: `argon2_erl`
 The API is really simple:
 
 ```erlang
-1> argon2:compare(<<"hello">>, argon2:hash(<<"hello">>)).
+1> argon2:verify(<<"hello">>, argon2:hash(<<"hello">>)).
 true
 ```
+
+There are also 2- and 3-arity versions of the `argon2:hash` function which provide for using a provided salt and different security options.
 
 ## Requirements
 
@@ -52,7 +54,7 @@ Note that we're not using this because we had difficulties getting it to build a
 
 Erlang code (c) 2017 James Laver, MIT LICENSE
 
-This code contains code licensed under other licenses:
+This library contains code licensed under other licenses:
 
 Argon2 is (c) Daniel Dinu and Dmitry Khovratovich, Apache 2.0 Licensed
 BLAKE2 is (c) Samuel Neves, 2013-2015, CC0 1.0 Licensed
--- src/argon2.erl
+++ src/argon2.erl
@@ -1,15 +1,58 @@
 -module(argon2).
 
--export([gen_salt/0, compare/2, hash/1, hash/2]).
+-export([gen_salt/0, verify/2, hash/1, hash/2, hash/3]).
+-export_type([argon2type/0]).
 
+%%% public api
+
+-type argon2type() :: argon2id | argon2i | argon2d.
+
+-spec gen_salt() -> binary().
 gen_salt() ->
   crypto:strong_rand_bytes(16).
 
-compare(Password,Hash) ->
-  ok.
+-spec verify(binary(), binary()) -> boolean().
+verify(Password,Hash) ->
+  H = binary:bin_to_list(Hash),
+  case argon2_nif:verify_nif(H, Password, type(Hash)) of
+    0 -> true;
+    _ -> false
+  end.
 
+-spec hash(binary()) -> binary().
 hash(Password) ->
   hash(Password, gen_salt()).
 
-hash(Password,Salt) ->
-  ok.
+-spec hash(binary(), binary()) -> binary().
+hash(Password, Salt) ->
+  hash(Password, Salt, #{}).
+
+-spec hash(binary(), binary(), map()) -> binary().
+hash(Password, Salt, Opts) ->
+  Opts2={Time, Mem, Par, Len, Type} = opts(Opts),
+  ELen = argon2_nif:encodedlen_nif(Time, Mem, Par, byte_size(Salt), Len, Type),
+  Ret = argon2_nif:hash_nif(Time, Mem, Par, Password, Salt, 0 , Len, ELen, Type, 0),
+  result(Ret, Opts2).
+
+%%% internals
+
+-spec type(argon2type() | binary()) -> pos_integer().
+type(argon2id) -> 2;
+type(argon2i)  -> 1;
+type(argon2d)  -> 0;
+type(<<"$argon2id$", _/binary>>) -> 2;
+type(<<"$argon2i$",  _/binary>>) -> 1;
+type(<<"$argon2d$",  _/binary>>) -> 0.
+
+-spec opts(map()) -> {pos_integer(),pos_integer(),pos_integer(),pos_integer(), argon2type()}.
+opts(Opts) ->
+  {maps:get(time_cost, Opts, 6),
+   maps:get(memory_cost, Opts, 16),
+   maps:get(parallelism, Opts, 1),
+   maps:get(length, Opts, 32),
+   type(maps:get(type, Opts, argon2id))}.
+
+result(Result, _) when is_integer(Result) ->
+  erlang:error(binary:list_to_bin(argon2_nif:error_nif(Result)));
+result({[], Encoded}, _) -> binary:list_to_bin(Encoded).
+
